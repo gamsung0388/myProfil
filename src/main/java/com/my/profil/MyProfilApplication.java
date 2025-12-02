f627446a09e054f38590cab61b1589d9c87fbee7
Gamsung0388 <ryan9320@gmail.com>
2025-12-02 20:24:15 +0900

    server commit

---
+++ src/main/java/com/my/profil/MyProfilApplication.java
@@ -0,0 +1,13 @@
+package com.my.profil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProfilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProfilApplication.class, args);
+	}
+
+}
